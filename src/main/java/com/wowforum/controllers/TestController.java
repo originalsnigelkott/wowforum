f754fc660b5b4cba34e7c650665cb93d6f6cca0f
Per Dahlstedt <dahlstedt.per@gmail.com>
2020-12-26 12:28:50 +0100

    Initial commit

---
+++ src/main/java/com/wowforum/controllers/TestController.java
@@ -0,0 +1,16 @@
+package com.wowforum.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/test")
+public class TestController {
+    @GetMapping
+    public ResponseEntity<String> test() {
+        return ResponseEntity.ok("Test route works");
+    }
+}
---
+++ src/test/java/com/wowforum/WowforumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wowforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WowforumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
