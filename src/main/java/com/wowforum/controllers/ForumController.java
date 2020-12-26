6fa966e9a88f7fa887778cb7036d028f603bad25
snigelkott <dahlstedt.per@gmail.com>
2020-12-26 14:06:01 +0100

    Add get and post endpoints for forums

--- src/main/java/com/wowforum/controllers/ForumController.java
+++ src/main/java/com/wowforum/controllers/ForumController.java
@@ -1,9 +1,33 @@
 package com.wowforum.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.wowforum.entities.Forum;
+import com.wowforum.services.ForumService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/forums")
 public class ForumController {
+    private final String ENDPOINT_NAME = "/api/v1/forums/";
+
+    @Autowired
+    private ForumService forumService;
+
+    @GetMapping
+    public ResponseEntity<List<Forum>> getAllForums() {
+        var forums = forumService.getAllForums();
+        return ResponseEntity.ok(forums);
+    }
+
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Forum> createForum(@RequestBody Forum forum) {
+        var createdForum = forumService.createForum(forum);
+        var uri = URI.create(ENDPOINT_NAME + createdForum.getId());
+        return ResponseEntity.created(uri).body(createdForum);
+    }
 }
--- src/main/java/com/wowforum/services/ForumService.java
+++ src/main/java/com/wowforum/services/ForumService.java
@@ -1,7 +1,22 @@
 package com.wowforum.services;
 
+import com.wowforum.entities.Forum;
+import com.wowforum.repositories.ForumRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ForumService {
+    @Autowired
+    private ForumRepository forumRepository;
+
+    public List<Forum> getAllForums() {
+        return forumRepository.findAll();
+    }
+
+    public Forum createForum(Forum forum) {
+        return forumRepository.save(forum);
+    }
 }
