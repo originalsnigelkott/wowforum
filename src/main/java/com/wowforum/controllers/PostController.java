5431d8706ef4efb91448cffc2cc8aa6992a4f867
snigelkott <dahlstedt.per@gmail.com>
2020-12-26 16:37:26 +0100

    Add endpoint to get post by thread id

--- src/main/java/com/wowforum/controllers/PostController.java
+++ src/main/java/com/wowforum/controllers/PostController.java
@@ -1,9 +1,24 @@
 package com.wowforum.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.wowforum.entities.Post;
+import com.wowforum.services.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1")
 public class PostController {
+    @Autowired
+    private PostService postService;
+
+    @GetMapping("threads/{threadId}/posts")
+    public ResponseEntity<List<Post>> getPostsByThreadId(@PathVariable UUID threadId) {
+        var posts =  postService.getPostsByThreadId(threadId);
+        return ResponseEntity.ok(posts);
+    }
 }
--- src/main/java/com/wowforum/services/PostService.java
+++ src/main/java/com/wowforum/services/PostService.java
@@ -1,7 +1,21 @@
 package com.wowforum.services;
 
+import com.wowforum.entities.Post;
+import com.wowforum.exceptions.EntityNotFoundException;
+import com.wowforum.repositories.PostRepository;
+import com.wowforum.repositories.ThreadRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class PostService {
+    @Autowired
+    private PostRepository postRepository;
+
+    public List<Post> getPostsByThreadId(UUID threadId) {
+        return postRepository.findAllByThreadId(threadId);
+    }
 }
