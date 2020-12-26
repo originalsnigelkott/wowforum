7dad2c8b7a5cb1181da3a26daae33a616cf83188
snigelkott <dahlstedt.per@gmail.com>
2020-12-26 14:17:02 +0100

    Add endpoint to get forum by id

--- src/main/java/com/wowforum/controllers/ForumController.java
+++ src/main/java/com/wowforum/controllers/ForumController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/forums")
@@ -22,6 +23,12 @@ public class ForumController {
     public ResponseEntity<List<Forum>> getAllForums() {
         var forums = forumService.getAllForums();
         return ResponseEntity.ok(forums);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Forum> getForumById(@PathVariable UUID id) {
+        var forum = forumService.getForumById(id);
+        return ResponseEntity.ok(forum);
     }
 
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
--- src/main/java/com/wowforum/services/ForumService.java
+++ src/main/java/com/wowforum/services/ForumService.java
@@ -1,11 +1,13 @@
 package com.wowforum.services;
 
 import com.wowforum.entities.Forum;
+import com.wowforum.exceptions.EntityNotFoundException;
 import com.wowforum.repositories.ForumRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class ForumService {
@@ -14,6 +16,10 @@ public class ForumService {
 
     public List<Forum> getAllForums() {
         return forumRepository.findAll();
+    }
+
+    public Forum getForumById(UUID id) {
+        return forumRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("forum", "id"));
     }
 
     public Forum createForum(Forum forum) {
