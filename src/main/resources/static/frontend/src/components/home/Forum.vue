848b461f4a5b68fd2ae75f3780ab85b8c30116da
snigelkott <dahlstedt.per@gmail.com>
2020-12-30 10:56:20 +0100

    Add forum component

---
+++ src/main/resources/static/frontend/src/components/home/Forum.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h4>{{ forum.name }}</h4>
+  </div>
+</template>
+
+<script>
+import { Vue, Component, Prop } from 'vue-property-decorator';
+
+@Component()
+class Forum extends Vue {
+  @Prop({ required: true })
+  forum
+}
+
+export default Forum
+</script>
+
+<style lang="scss" scoped>
+
+</style>
