281bdd6e848a6f5d06fc69c07706187791eaf636
snigelkott <dahlstedt.per@gmail.com>
2020-12-30 10:56:42 +0100

    Add forum list component

---
+++ src/main/resources/static/frontend/src/components/home/ForumList.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="forum-list">
+    <div class="header">
+      <h3 class="title">Forums</h3>
+    </div>
+    <div class="forums">
+      <Forum v-for="forum of forums" :key="forum.id" :forum="forum" />
+    </div>
+  </div>
+</template>
+
+<script>
+import { Vue, Component } from "vue-property-decorator";
+import { BASE_URL } from "../../app-strings";
+import Forum from "./Forum";
+
+@Component({
+  components: { Forum },
+})
+class ForumList extends Vue {
+  forums = [];
+
+  async created() {
+    const data = await fetch(`${BASE_URL}/forums`);
+    try {
+      const forums = await data.json();
+      this.forums = forums;
+      console.log(this.forums);
+    } catch {
+      console.log("Error data.json().");
+    }
+  }
+}
+
+export default ForumList;
+</script>
+
+<style lang="scss" scoped>
+.forum-list {
+  width: 100%;
+  border: black solid 1px;
+  .header {
+    width: 100%;
+    padding: 10px;
+    border-bottom: black solid 1px;
+    .title {
+      font-size: 24px;
+      color: hotpink;
+    }
+  }
+  .forums {
+    padding: 10px;
+  }
+}
+</style>
