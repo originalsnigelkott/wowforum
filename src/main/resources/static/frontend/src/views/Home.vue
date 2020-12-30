eba3b40e2fe77fbdd0f6b79437069f7b07029869
snigelkott <dahlstedt.per@gmail.com>
2020-12-30 10:57:31 +0100

    Add forum list to home view

--- src/main/resources/static/frontend/src/views/Home.vue
+++ src/main/resources/static/frontend/src/views/Home.vue
@@ -1,22 +1,21 @@
 <template>
-  <div>
-    <h2>Home</h2>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam dicta ut minima architecto ducimus totam? Obcaecati voluptate, quam asperiores repellat distinctio delectus laborum nihil magnam ratione aperiam, adipisci quos cupiditate vero eum. Iste quod, fugit ea impedit asperiores quis consequuntur maiores quasi eos eum tempore excepturi dolorum officiis deserunt placeat!</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam dicta ut minima architecto ducimus totam? Obcaecati voluptate, quam asperiores repellat distinctio delectus laborum nihil magnam ratione aperiam, adipisci quos cupiditate vero eum. Iste quod, fugit ea impedit asperiores quis consequuntur maiores quasi eos eum tempore excepturi dolorum officiis deserunt placeat!</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam dicta ut minima architecto ducimus totam? Obcaecati voluptate, quam asperiores repellat distinctio delectus laborum nihil magnam ratione aperiam, adipisci quos cupiditate vero eum. Iste quod, fugit ea impedit asperiores quis consequuntur maiores quasi eos eum tempore excepturi dolorum officiis deserunt placeat!</p>
+  <div class="view">
+    <ForumList />
   </div>
 </template>
 
 <script>
-import { Vue, Component } from 'vue-property-decorator';
+import { Vue, Component } from "vue-property-decorator";
+import ForumList from "../components/home/ForumList";
 
-@Component()
-class Home extends Vue {
-  
-}
+@Component({
+  components: { ForumList },
+})
+class Home extends Vue {}
 
-export default Home
+export default Home;
 </script>
 
-<style>
+<style lang="scss" scoped>
+
 </style>
