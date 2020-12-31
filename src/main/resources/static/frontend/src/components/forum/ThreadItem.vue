a1d93813efed39b8d08e43b05e194ca04267114a
Per Dahlstedt <dahlstedt.per@gmail.com>
2020-12-31 09:31:35 +0100

    Add thread components

---
+++ src/main/resources/static/frontend/src/components/forum/ThreadItem.vue
@@ -0,0 +1,45 @@
+<template>
+  <div
+    class="thread"
+    :class="{ pointer: canNavigate }"
+    @click="canNavigate && navigate()"
+  >
+    <div class="header">
+      <h4 class="title" :style="styleObject">{{ thread.topic }}</h4>
+    </div>
+  </div>
+</template>
+
+<script>
+import { Vue, Component, Prop } from "vue-property-decorator";
+
+@Component()
+class ThreadItem extends Vue {
+  @Prop({ required: true })
+  thread;
+  @Prop({ default: 24 })
+  fontSize;
+  @Prop({ default: true })
+  canNavigate;
+
+  get styleObject() {
+    return { fontSize: this.fontSize + "px" };
+  }
+
+  navigate() {
+    // TODO: Implement navigate method
+  }
+}
+
+export default ThreadItem;
+</script>
+
+<style lang="scss" scoped>
+.thread {
+  margin: 5px 0;
+  border: turquoise solid 1px;
+  .header {
+    padding: 5px 10px;
+  }
+}
+</style>
