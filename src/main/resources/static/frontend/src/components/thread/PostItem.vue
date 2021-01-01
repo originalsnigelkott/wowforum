8f4218ab46736c09ecd84754b241272ebd72e274
Per Dahlstedt <dahlstedt.per@gmail.com>
2021-01-01 12:45:10 +0100

    Add component

---
+++ src/main/resources/static/frontend/src/components/thread/PostItem.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="post">
+    <div class="header row">
+      <span>
+        Author: <span class="author">{{ post.creator.username }}</span>
+      </span>
+      <span>
+        Created: <span class="created">{{ creationDate }}</span>
+      </span>
+    </div>
+    <div class="content">
+      <p>{{ post.content }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import { Vue, Component, Prop } from "vue-property-decorator";
+
+@Component()
+class PostItem extends Vue {
+  @Prop({ required: true })
+  post;
+
+  get creationDate() {
+    return new Date(this.post.created).toLocaleString();
+  }
+}
+
+export default PostItem;
+</script>
+
+<style lang="scss" scoped>
+.post {
+  margin: 5px 0;
+  border: turquoise solid 1px;
+  .header {
+    padding: 5px 10px;
+    border-bottom: turquoise solid 1px;
+    justify-content: space-between;
+    .author,
+    .created {
+      font-weight: bold;
+      color: turquoise;
+    }
+  }
+  .content {
+    padding: 5px 10px;
+  }
+}
+</style>
