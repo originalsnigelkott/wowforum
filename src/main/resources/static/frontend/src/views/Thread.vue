7467d82c9cfeaaac343d0b9d2cb40afd94b93e87
Per Dahlstedt <dahlstedt.per@gmail.com>
2021-01-01 12:46:21 +0100

    Add list of posts to thread view

--- src/main/resources/static/frontend/src/views/Thread.vue
+++ src/main/resources/static/frontend/src/views/Thread.vue
@@ -1,16 +1,38 @@
 <template>
   <div class="view">
-    
+    <div class="content">
+      <div class="header row">
+        <h2 class="title">{{ thread.topic }}</h2>
+        <div class="meta-data row">
+          <span>
+            Created by: <span class="author">{{ author }}</span>
+          </span>
+          <span>
+            Created at: <span class="created">{{ creationDate }}</span>
+          </span>
+        </div>
+      </div>
+      <PostList :posts="thread.posts" />
+    </div>
   </div>
 </template>
 
 <script>
-import { Vue, Component } from 'vue-property-decorator';
-import { BASE_URL } from '../app-strings';
+import { Vue, Component } from "vue-property-decorator";
+import { BASE_URL } from "../app-strings";
+import PostList from "../components/thread/PostList";
 
-@Component()
+@Component({ components: { PostList } })
 class Thread extends Vue {
   thread = {};
+
+  get author() {
+    return this.thread?.initialPost?.creator?.username;
+  }
+
+  get creationDate() {
+    return new Date(this.thread?.initialPost?.created).toLocaleString();
+  }
 
   async created() {
     const threadId = this.$route.params.id;
@@ -18,14 +40,36 @@ class Thread extends Vue {
     try {
       const thread = await data.json();
       this.thread = thread;
+      console.log(thread);
     } catch {
       console.error("An error occured while loading the thread.");
     }
   }
 }
 
-export default Thread
+export default Thread;
 </script>
 
 <style lang="scss" scoped>
+.content {
+  border: turquoise solid 1px;
+  .header {
+    padding: 10px;
+    border-bottom: turquoise solid 1px;
+    justify-content: space-between;
+    .title {
+      font-size: 24px;
+    }
+    .meta-data {
+      align-items: center;
+      .author {
+        margin-right: 4px;
+      }
+      .author, .created {
+        color: turquoise;
+        font-weight: bold;
+      }
+    }
+  }
+}
 </style>
