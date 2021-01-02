1c95b0deaaffb819194fc4b95176198eede07ebe
snigelkott <dahlstedt.per@gmail.com>
2021-01-02 14:29:06 +0100

    Add post user endpoint and ability to save users

--- src/main/java/com/wowforum/controllers/UserController.java
+++ src/main/java/com/wowforum/controllers/UserController.java
@@ -1,9 +1,33 @@
 package com.wowforum.controllers;
 
+import com.wowforum.dtos.UserCreateDto;
+import com.wowforum.dtos.UserReadDto;
+import com.wowforum.entities.Forum;
+import com.wowforum.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.URI;
+
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/users")
 public class UserController {
+    private static final String ENDPOINT_NAME = "/api/v1/users";
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<UserReadDto> createForum(@RequestBody UserCreateDto user) {
+        var createdUser = userService.createUser(user);
+        var uri = URI.create(ENDPOINT_NAME + createdUser.getId());
+        var dto = new UserReadDto(createdUser);
+        return ResponseEntity.created(uri).body(dto);
+    }
+
 }
--- src/main/java/com/wowforum/services/UserService.java
+++ src/main/java/com/wowforum/services/UserService.java
@@ -1,7 +1,33 @@
 package com.wowforum.services;
 
+import com.wowforum.dtos.UserCreateDto;
+import com.wowforum.entities.User;
+import com.wowforum.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User createUser(UserCreateDto userCreateDto) {
+        var user = new User(userCreateDto);
+        user.setPassword(passwordEncoder.encode(userCreateDto.getPassword()));
+        if(currentUserIsAdmin()) {
+            user.setRoles(userCreateDto.getRoles());
+        } else {
+            user.setRoles("USER");
+        }
+        return userRepository.save(user);
+    }
+
+    private boolean currentUserIsAdmin() {
+        // TODO: change to check the current user role
+        return true;
+    }
 }
