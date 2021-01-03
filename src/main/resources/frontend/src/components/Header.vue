58bd263e8a32c30891866689d3bc54e1b2f5ba08
snigelkott <dahlstedt.per@gmail.com>
2021-01-03 22:30:02 +0100

    Fix button placement and add logout functionality

--- src/main/resources/frontend/src/components/Header.vue
+++ src/main/resources/frontend/src/components/Header.vue
@@ -1,8 +1,22 @@
 <template>
-  <div class="header row center-xy">
-    <h1 @click="goHome()" class="title pointer">Wow forum</h1>
-    <button v-if="shouldShowLoginButton" @click="goToLogin()" class="auth-btn btn">Login</button>
-    <button v-if="shouldShowLogoutButton" @click="logout()" class="auth-btn btn">Logout</button>
+  <div class="header">
+    <div class="header-content row center-xy">
+      <h1 @click="goHome()" class="title pointer">Wow forum</h1>
+      <button
+        v-if="shouldShowLoginButton"
+        @click="goToLogin()"
+        class="auth-btn btn"
+      >
+        Login
+      </button>
+      <button
+        v-if="shouldShowLogoutButton"
+        @click="logout()"
+        class="auth-btn btn"
+      >
+        Logout
+      </button>
+    </div>
   </div>
 </template>
 
@@ -12,11 +26,11 @@ import { Vue, Component } from "vue-property-decorator";
 @Component()
 class Header extends Vue {
   get shouldShowLoginButton() {
-    return this.$route.name != "Login" && !this.$store.currentUser;
+    return this.$route.name != "Login" && !this.$store.state.currentUser;
   }
-  
+
   get shouldShowLogoutButton() {
-    return this.$store.currentUser;
+    return this.$store.state.currentUser;
   }
 
   goHome() {
@@ -25,6 +39,10 @@ class Header extends Vue {
 
   goToLogin() {
     if (this.$route.name != "Login") this.$router.push({ name: "Login" });
+  }
+
+  logout() {
+    this.$store.dispatch("logout");
   }
 }
 
@@ -40,21 +58,26 @@ export default Header;
   right: 0;
   z-index: 999;
   background-color: turquoise;
-  padding: 0 50px;
-  .title {
-    padding: 10px;
-    &:hover {
-      box-shadow: none;
+  .header-content {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    padding: 0 50px;
+    .title {
+      padding: 10px;
+      &:hover {
+        box-shadow: none;
+      }
     }
-  }
-  .auth-btn {
-    position: absolute;
-    right: 50px;
-    background-color: hotpink;
-    color: turquoise;
-    font-weight: bold;
-    &:hover {
-      background-color: rgba(255, 105, 180, 0.938);
+    .auth-btn {
+      position: fixed;
+      right: 50px;
+      background-color: hotpink;
+      color: turquoise;
+      font-weight: bold;
+      &:hover {
+        background-color: rgba(255, 105, 180, 0.938);
+      }
     }
   }
 }
