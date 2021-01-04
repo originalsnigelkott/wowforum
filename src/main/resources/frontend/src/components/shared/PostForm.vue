f0d9583fa053f85099f8767ce48a289698f7b953
snigelkott <dahlstedt.per@gmail.com>
2021-01-04 15:26:18 +0100

    Add PostForm

---
+++ src/main/resources/frontend/src/components/shared/PostForm.vue
@@ -0,0 +1,51 @@
+<template>
+  <form class="form" @submit.prevent="createPost()">
+    <div class="input-fields col">
+      <label for="content-input">Content</label>
+      <textarea
+        id="content-input"
+        class="input-field"
+        v-model="content"
+        rows="4"
+        cols="50"
+        autofocus
+        required
+      ></textarea>
+    </div>
+    <button :disabled="processing" type="submit" class="btn submit-btn">
+      Create post
+    </button>
+  </form>
+</template>
+
+<script>
+import { Vue, Component } from "vue-property-decorator";
+import { BASE_VERSION_URL } from "@/app-strings";
+import { fetchWithCredentials } from "@/utils";
+
+@Component()
+class CreatePostForm extends Vue {
+  processing = false;
+  content = null;
+
+  async createPost() {
+    console.log(this.content);
+    const threadId = this.$route.params.id;
+    const response = await fetchWithCredentials(
+      `${BASE_VERSION_URL}/threads/${threadId}/posts`,
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ content: this.content }),
+      }
+    );
+    console.log(response);
+  }
+}
+
+export default CreatePostForm;
+</script>
+
+<style lang="scss" scoped>
+@import "@/styles/form";
+</style>
