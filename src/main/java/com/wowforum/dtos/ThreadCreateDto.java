f83db272dccffd06fadb23d20469fdfb31ac4fab
snigelkott <dahlstedt.per@gmail.com>
2021-01-05 10:08:25 +0100

    Add ThreadCreateDto

---
+++ src/main/java/com/wowforum/dtos/ThreadCreateDto.java
@@ -0,0 +1,13 @@
+package com.wowforum.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ThreadCreateDto {
+    private String topic;
+    private PostCreateDto initialPost;
+}
