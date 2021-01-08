d358f71456dcf82c5a169471751ec8aed80ad84d
Per Dahlstedt <dahlstedt.per@gmail.com>
2021-01-08 14:13:23 +0100

    Add util function

--- src/main/resources/frontend/src/utils.js
+++ src/main/resources/frontend/src/utils.js
@@ -1,4 +1,13 @@
+import store from "@/store/index";
+
 export async function fetchWithCredentials(url, opts) {
   const response = await fetch(url, { credentials: "include", ...opts });
   return response;
+}
+
+export function hasModerationRights(forumId) {
+  return (
+    store.getters.currentUser?.roles.includes("ADMIN") ||
+    store.getters.currentUser?.moderates.includes(forumId)
+  );
 }
