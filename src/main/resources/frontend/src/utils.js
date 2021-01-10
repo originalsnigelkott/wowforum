5a2edbe39438eddf4575fb9376aa65b6774732e6
Per Dahlstedt <dahlstedt.per@gmail.com>
2021-01-10 11:13:40 +0100

    Refactor utils

--- src/main/resources/frontend/src/utils.js
+++ src/main/resources/frontend/src/utils.js
@@ -1,13 +1,14 @@
 import store from "@/store/index";
 
 export async function fetchWithCredentials(url, opts) {
-  const response = await fetch(url, { credentials: "include", ...opts });
+  const response = await fetch(url, { ...opts, credentials: "include" });
   return response;
 }
 
 export function hasModerationRights(forumId) {
+  const currentUser = store.getters.currentUser;
   return (
-    store.getters.currentUser?.roles.includes("ADMIN") ||
-    store.getters.currentUser?.moderates.includes(forumId)
+    currentUser?.roles.includes("ADMIN") ||
+    currentUser?.moderates.includes(forumId)
   );
 }
