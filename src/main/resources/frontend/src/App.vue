ddadcb6725318ed3bc88656fc0269369210182c0
Per Dahlstedt <dahlstedt.per@gmail.com>
2021-01-10 12:47:46 +0100

    Add dispatch when creating app

--- src/main/resources/frontend/src/App.vue
+++ src/main/resources/frontend/src/App.vue
@@ -14,6 +14,7 @@ import Header from "@/components/Header.vue";
 })
 class App extends Vue {
   async created() {
+    await this.$store.dispatch("fetchForums");
     await this.$store.dispatch("getCurrentUser");
   }
 }
