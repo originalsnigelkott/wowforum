0bd79c3519dc6e21af2da86fcf46742cc4b3043c
Per Dahlstedt <dahlstedt.per@gmail.com>
2021-01-10 12:32:13 +0100

    Add layout and styling

--- src/main/resources/frontend/src/components/admin/UserItem.vue
+++ src/main/resources/frontend/src/components/admin/UserItem.vue
@@ -1,6 +1,21 @@
 <template>
-  <div class="user">
-    {{ user.username }}
+  <div class="user col">
+    <div class="user-info row center-y">
+      <h3 class="username">
+        {{ user.username }}
+      </h3>
+      <div class="names col">
+        <span class="firstname">
+          {{ user.firstName }}
+        </span>
+        <span class="lastname">
+          {{ user.lastName }}
+        </span>
+      </div>
+    </div>
+    <div class="user-controls">
+      Controls go here
+    </div>
   </div>
 </template>
 
@@ -16,4 +31,25 @@ class UserItem extends Vue {
 export default UserItem;
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.user {
+  margin: 5px 0;
+  border: turquoise solid 1px;
+  .user-info {
+    padding: 5px;
+    border-bottom: turquoise solid 1px;
+    .username {
+      font-size: 24px;
+      margin-right: 10px;
+    }
+    .names {
+      .firstname, .lastname {
+        font-size: 14px;
+      }
+    }
+  }
+  .user-controls {
+    padding: 5px;
+  }
+}
+</style>
