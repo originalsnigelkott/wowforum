d30d9c57ff4c16717638a84cc15c02b20c8a6366
Per Dahlstedt <dahlstedt.per@gmail.com>
2021-01-12 09:41:28 +0100

    Set user to null when getting 401 status code

--- src/main/resources/frontend/src/utils.js
+++ src/main/resources/frontend/src/utils.js
@@ -2,6 +2,9 @@ import store from "@/store/index";
 
 export async function fetchWithCredentials(url, opts) {
   const response = await fetch(url, { ...opts, credentials: "include" });
+  if(response.status === 401) {
+    store.commit("setCurrentUser", null);
+  }
   return response;
 }
 
