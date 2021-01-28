aacd6bf3a0511d84452748f80f41032fdeab4273
Per Dahlstedt <dahlstedt.per@gmail.com>
2021-01-28 16:00:17 +0100

    Removed exception handler

--- src/main/java/com/wowforum/configs/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/wowforum/configs/CustomAuthenticationEntryPoint.java
@@ -25,10 +25,4 @@ public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint 
                        AccessDeniedException accessDeniedException) throws IOException {
     response.sendError(HttpServletResponse.SC_FORBIDDEN, "Authorization Failed : " + accessDeniedException.getMessage());
   }
-
-  @ExceptionHandler (value = {Exception.class})
-  public void commence(HttpServletRequest request, HttpServletResponse response,
-                       Exception exception) throws IOException {
-    response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Internal Server Error : " + exception.getMessage());
-  }
 }
