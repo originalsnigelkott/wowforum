e938f7e595d5611c11ea170c83f3064248efcfdb
Per Dahlstedt <dahlstedt.per@gmail.com>
2021-01-29 12:34:59 +0100

    Add doc server urls

--- src/main/java/com/wowforum/WowforumApplication.java
+++ src/main/java/com/wowforum/WowforumApplication.java
@@ -1,8 +1,22 @@
 package com.wowforum;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+  servers = {
+    @Server(
+      description = "Live url",
+      url = "https://wow-en.hakkerbojz.tech"
+    ),
+    @Server(
+      description = "Dev url",
+      url = "http://localhost:3000"
+    )
+  }
+)
 @SpringBootApplication
 public class WowforumApplication {
   public static void main(String[] args) {
